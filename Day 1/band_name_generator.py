306502a9323243305d82fb6bc90780433a2ba860
Juan Pablo Cabrera <jpcq2002@yahoo.com.mx>
2024-07-30 18:09:32 -0600

    day one project

---
+++ Day 1/band_name_generator.py
@@ -0,0 +1,5 @@
+print("Welcome to the Band Name Generator.")
+input_city = input("What's the name of the city you grew up in?\n")
+pet = input("What's the name of your first pet?\n")
+
+print("Your band will be named " + input_city + " " + pet)
